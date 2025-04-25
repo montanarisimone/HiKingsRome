579f4f41e5eb9264f8b0a1c26e60e248388b7600
Simone Montanari <40363581+montanarisimone@users.noreply.github.com>
2025-04-25 13:07:09 +0200

    Update db_keyboards.py

--- Hiky_the_bot/utils/db_keyboards.py
+++ Hiky_the_bot/utils/db_keyboards.py
@@ -54,6 +54,7 @@ class KeyboardBuilder:
             [InlineKeyboardButton("Create new hike 🏔️", callback_data='admin_create_hike')],
             [InlineKeyboardButton("Manage existing hikes 📝", callback_data='admin_manage_hikes')],
             [InlineKeyboardButton("Schedule maintenance 🔧", callback_data='admin_maintenance')],
+            [InlineKeyboardButton("Query Database 🔍", callback_data='query_db')],
             [InlineKeyboardButton("Add admin 👑", callback_data='admin_add_admin')],
             [InlineKeyboardButton("🔙 Back to menu", callback_data='back_to_menu')]
         ]
